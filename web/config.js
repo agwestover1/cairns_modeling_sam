cbf0b8de4fc05b8dfe43aa53384549be3b6179e5
agwestover1 <agwestover1@gmail.com>
2026-05-29 15:58:44 -0400

    Update config.js

--- web/config.js
+++ web/config.js
@@ -2,4 +2,4 @@
  * Get a free token at https://account.mapbox.com/access-tokens (50k loads/month free).
  * Paste it below, then refresh /web/3d.html.
  */
-window.MAPBOX_TOKEN = "";
+window.MAPBOX_TOKEN = "pk.eyJ1IjoiYWd3ZXN0b3ZlcjEiLCJhIjoiY21wcXY2NHlqMHE2cDJxcHIxaWF0aTJ2bSJ9.IuJaQAxubSREP7fzG2jSww";
